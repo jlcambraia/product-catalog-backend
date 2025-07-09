206eb45b41af1753de0bafdf934bee6e9a148fb5
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-07-09 13:13:39 -0300

    feat: add schema product.js

---
+++ models/product.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlenght: 30,
+    },
+    price: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return v >= 0;
+        },
+        message: "O preço deve ser maior ou igual a zero",
+      },
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlenght: 300,
+    },
+    image: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v, { require_protocol: true }),
+      },
+    },
+    category: {
+      type: String,
+      enum: ["Acessórios", "Móveis", "Eletrônicos", "Roupas", "Outros"],
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return v >= 0;
+        },
+        message: "O estoque deve ser maior ou igual a zero",
+      },
+    },
+  },
+  { versionKey: false }
+);
+
+const Produto = mongoose.model("product", productSchema);
+
+export default Produto;
