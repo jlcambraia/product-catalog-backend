14760d47e27183e9f2520d2e73704f51472da04d
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-07-09 13:13:28 -0300

    feat: add controller products.js

---
+++ controllers/products.js
@@ -0,0 +1,50 @@
+import Produto from "../models/product.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const allProducts = await Produto.find();
+    res.send({ data: allProducts });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const { name, price, description, image, category, stock } = req.body;
+
+  if (!name || !price || !description || !image || !category || !stock) {
+    return res
+      .status(400)
+      .send({ message: "Os dados fornecidos são inválidos" });
+  }
+
+  try {
+    const newProduct = await Produto.create({
+      name,
+      price,
+      description,
+      image,
+      category,
+      stock,
+    });
+    res.send({ data: newProduct });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const deleted = await Produto.findById(productId);
+
+    if (!deleted) {
+      return res.status(404).send({ message: "Produto não encontrado" });
+    }
+
+    res.status(200).send({ message: "Produto deletado com sucesso" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
